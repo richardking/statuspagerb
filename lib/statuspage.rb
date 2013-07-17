def4fc4be5edd67404f63a160d0c50c365e1a915
Richard King <rkingucla@ymail.com>
2013-07-16 17:57:10 -0700

    implement profile, components and incidents methods

--- lib/statuspage.rb
+++ lib/statuspage.rb
@@ -1,11 +1,13 @@
 require 'rubygems'
-require 'open-uri'
+require 'httparty'
 
 class StatusPage
-  attr_reader :oauth
+  STATUSES = %w(operational degraded_performance partial_outage major_outage)
 
   def initialize(args=nil)
     @oauth = "adeac0389fd4fb8a1519c2590f36263708e9574c6d5560f7e6808acfe1261ffe"
+    @base_url = "https://api.statuspage.io/v1/pages/"
+    @page = "p9qz8c9gtcnh"
 
     start(args) if args
   end
@@ -18,15 +20,45 @@ class StatusPage
     else
       puts "Command not recognized"
     end
-    puts command
-    puts args
   end
 
   def profile(*args)
-    puts `curl https://api.statuspage.io/v1/pages/#{args[0]}.json -H "Authorization: OAuth #{@oauth}"`
-    puts "XXX"
-    puts args.inspect
-    puts args.class
+    results = httparty_send :get, "#{@base_url}#{@page}.json"
+    puts results
+    puts results["allow_sms_subscribers"]
   end
 
+  def components(*args)
+    if args.empty?
+      results = httparty_send :get, "#{@base_url}#{@page}/components.json"
+      puts results
+    else
+      if STATUSES.include? args[1]
+        results = httparty_send :patch, "#{@base_url}#{@page}/components/#{args[0]}.json", :body => {"component[status]" => args[1]}
+        puts results
+      else
+        puts "Invalid status"
+      end
+    end
+  end
+
+  def incidents(*args)
+    if args.empty?
+      results = httparty_send :get, "#{@base_url}#{@page}/incidents.json"
+      puts results
+    else
+      options = { :body => { "incident[name]" => args[0], "incident[status]" => args[1],  "incident[message]" => args[3] } }
+      results = httparty_send :post, "#{@base_url}#{@page}/incidents.json", options
+      puts results
+    end
+  end
+
+  private
+
+  def httparty_send(action, url, options={})
+    puts url
+    options.merge!(:headers => { "Authorization: OAuth" => @oauth })
+    puts "OPT: #{options}"
+    HTTParty.send(action, url, options)
+  end
 end
